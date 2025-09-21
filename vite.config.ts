c2c02913cfc0e3d57407a0cec46c252de4525586
Abdallaheng2022 <106319055+Abdallaheng2022@users.noreply.github.com>
2025-09-21 16:06:04 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // Update `base` to '/<your-repo-name>/' if deploying under a subpath on GitHub Pages.
 export default defineConfig({
   plugins: [react()],
-  base: '/', // e.g., '/abdallah-portfolio/'
+  base: '/abdallah-portfolio/', // e.g., '/abdallah-portfolio/'
 })
